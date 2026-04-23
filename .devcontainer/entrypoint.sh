ff5c7522c6f76ab257d024dca98619134c05ee39
Matej Stajduhar <zdravko@modul8.dev>
2026-04-23 20:08:51 +0000

    added codex to devcontainer

---
+++ .devcontainer/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+ensure_owned() {
+  target="$1"
+  if [ -z "$target" ]; then
+    return
+  fi
+
+  if [ "$(id -u)" -ne 0 ]; then
+    sudo mkdir -p "$target"
+    sudo chown -R vscode:vscode "$target"
+  else
+    mkdir -p "$target"
+    chown -R vscode:vscode "$target"
+  fi
+}
+
+if [ -d /home/vscode ]; then
+  ensure_owned /home/vscode/.codex
+  ensure_owned /home/vscode/.config/Code/User/globalStorage
+  ensure_owned /home/vscode/.config/Code/User/globalStorage/openai.chatgpt
+fi
+
+exec "$@"
